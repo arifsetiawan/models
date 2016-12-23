68020046a947a3d8edf462bd61615ce487e85b39
irfan nurhakim <myfunareaa@gmail.com>
2016-12-23 14:40:58 +0700

    Update tenant

--- tenant.go
+++ tenant.go
@@ -2,8 +2,15 @@ package models
 
 // Tenant is a struct representation for Tenant Object
 type Tenant struct {
-	Type        string `json:"_type,omitempty"`
-	TenantID    string `json:"tenant_id"`
-	Name        string `json:"tenant_name"`
-	Description string `json:"tenant_description,omitempty"`
+	Type                  string               `json:"_type,omitempty"`
+	TenantID              string               `json:"tenant_id"`
+	Name                  string               `json:"tenant_name"`
+	Description           string               `json:"tenant_description,omitempty"`
+	DefaultAppCollections []TenantApplications `json:"default_app_collection"`
+}
+
+// TenantApplications is a representation for tenant's default app
+type TenantApplications struct {
+	AppType string `json:"app_type"`
+	AppID   string `json:"app_id"`
 }
