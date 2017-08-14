62be6de6f8d9e02cec479917089ed34467f90625
Nurul Arif Setiawan <n.arif.setiawan@gmail.com>
2017-08-14 15:22:52 +0700

    add position

---
+++ position.go
@@ -0,0 +1,21 @@
+package models
+
+// PositionJSON is
+type PositionJSON struct {
+	Type     string `json:"_type,omitempty"`
+	TenantID string `json:"tenant_id,omitempty"`
+	ID       string `json:"id"`
+	GroupID  string `json:"group_id"`
+	ParentID string `json:"parent_id"`
+}
+
+// PositionGroupJSON is
+type PositionGroupJSON struct {
+	Type       string `json:"_type,omitempty"`
+	TenantID   string `json:"tenant_id,omitempty"`
+	ID         string `json:"id"`
+	GroupID    string `json:"group_id"`
+	GroupName  string `json:"group_name"`
+	ParentID   string `json:"parent_id"`
+	ParentName string `json:"parent_name"`
+}
